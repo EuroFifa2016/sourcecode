c3bba6d14ddaf30ce18c5a79f431bb373209dea6
Sandeep kumar <sandeepkumar@Trigma-Mac-Mini.local>
2016-03-15 19:46:58 +0530

    ScorerViewController

--- Euro2016App/ScorerViewController.swift
+++ Euro2016App/ScorerViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class ScorerViewController: UIViewController {
+class ScorerViewController: ActivityIndicatorViewController,UITableViewDataSource {
     
     // MARK: Properties
     
-    var scorers = [scorer]()
+    @IBOutlet weak var tableView_Score: UITableView!
+    var scorers = [PlayerDetail]()
+    var arr_ScoreDetail = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self .loadScorerPlayers()
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self .loadScorerPlayers()
+        self.navigationController?.navigationBar.topItem?.title = "News"
+        
+        
+        
+        //Remove  values from Instance class
+        
+        playerdetails.playerImage = ""
+        playerdetails.playerName = ""
+        playerdetails.weight = ""
+        playerdetails.height =  ""
+        playerdetails.birthDate = ""
+        playerdetails.nationality = ""
+        playerdetails.nationFlag = ""
+        playerdetails.teamFlag = ""
+        playerdetails.team = ""
+        
+        playerdetails.position = ""
+        playerdetails.shirtNumber = ""
+        // End of Code
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,46 +56,135 @@ class ScorerViewController: UIViewController {
 
     func loadScorerPlayers(){
         
-        let scorerphoto1 = UIImage(named: "Image16")
-        let  scorer1 = scorer(scorerName: "Messi", countryName: "Argentina", scorerPhoto: scorerphoto1!, mgPoint: "23", miss_goal: "12 min")
-        
-        let scorerphoto2 = UIImage(named: "img2")
-        let  scorer2 = scorer(scorerName: "Ronaldo", countryName: "Germany", scorerPhoto: scorerphoto2!, mgPoint: "22", miss_goal: "89 min")
-        scorers += [scorer1,scorer2]
-    }
+        super.progressBarDisplayer(true, view: self.view)
+        DataManager.GETAPI("getTopScorers") { (response) -> Void in
+            super.progressBarDisplayer(false, view: self.view)
+            if response.objectForKey("result") as? Bool == true {
+                
+                print(response)
+//                
+//                for index in response.objectForKey("playersInfo") as! NSArray{
+//                    
+//                   // PlayerDetail.playerImage =
+//                   
+//                    
+////                    playerdetails.playerImage = (index.objectForKey("player_image") ?? "") as! String
+////                    
+////                    playerdetails.playerName = (index.objectForKey("player_name") ?? "") as! String
+////                    playerdetails.weight = (index.objectForKey("weight") ?? "") as! String
+////                     playerdetails.height = (index.objectForKey("height") ?? "") as! String
+////                     playerdetails.birthDate = (index.objectForKey("dob") ?? "") as! String
+////                     playerdetails.nationality = (index.objectForKey("nationality") ?? "") as! String
+////                     playerdetails.nationFlag = (index.objectForKey("nationality") ?? "") as! String
+////                     playerdetails.teamFlag = (index.objectForKey("team_flag") ?? "")as! String
+////                     playerdetails.team = (index.objectForKey("team_name") ?? "") as! String
+////                     playerdetails.position = (index.objectForKey("position") ?? "") as! String
+////                    playerdetails.shirtNumber = (index.objectForKey("shirt_number") ?? "") as! String
+//                    
+//                    let scroreObj = PlayerDetail(playerImage: playerImage, playerName: fullName, weight: weight, height: height,birthDate: birthDate, nationality: nation, nationFlag: nationFlag, teamFlag: teamFlag, team: team, position: position, shirtNumber: shirtNumber)
+//
+//                    
+//                    self.scorers += [scroreObj]
+//
+//                }
+//                
 
-
-    // delegates of tableView
-
-
-        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
-            return scorers.count
+                
+                self.arr_ScoreDetail = response.valueForKey("playersInfo") as! NSMutableArray
+                
+                if self.arr_ScoreDetail .count > 0
+                {
+                    self.tableView_Score .reloadData()
+                }
+                self.tableView_Score.reloadData()
+            }
         }
-
-
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            // Table view cells are reused and should be dequeued using a cell identifier.
-            let cellIdentifier = "ScorerCell"
-            let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ScorerCell
-            cell.accessoryView = UIImageView(image: (UIImage(named: "list-bg")))
-            let scorer = scorers[indexPath.row]
-            cell.playerName.text = scorer.scorerName
-            cell.playerImage.image = scorer.scorerPhoto
-            cell.countryName.text = scorer.countryName
-            cell.mgLabel.text = scorer.mgPoint
-            cell.miss_goalLabel.text = scorer.miss_goal
-            if indexPath.row%2==0
-            {
-                cell.backgroundColor = UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1)
-            }
-            else
-            {
-                cell.backgroundColor = UIColor.whiteColor()
-            }
-
-            return cell
-
     }
+    
+    
+    // delegates of tableView
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.arr_ScoreDetail.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "ScorerCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ScorerCell
+        cell.accessoryView = UIImageView(image: (UIImage(named: "list-bg")))
+        //let scorer = scorers[indexPath.row]
+        cell.playerName.text = self.arr_ScoreDetail[indexPath.row].valueForKey("player_name") as? String
+        cell.mgLabel.text = self.arr_ScoreDetail[indexPath.row].valueForKey("goals") as? String
+        cell.countryName.text = self.arr_ScoreDetail[indexPath.row].valueForKey("nationality") as? String
+        cell.miss_goalLabel.text = self.arr_ScoreDetail[indexPath.row].valueForKey("mins_goals") as? String
+        
+        let url = NSURL(string: (self.arr_ScoreDetail[indexPath.row].valueForKey("player_image") as? String)!)
+       // let data = NSData(contentsOfURL: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check
+    //     let image = UIImage(data: data!)
+        
+       cell.playerImage.sd_setImageWithURL(url)
+     //   cell.playerImage.image = image
+//        cell.playerImage.backgroundColor = UIColor.redColor()
+//        cell.playerImage.layer.cornerRadius = cell.playerImage.frame.size.width / 2;
+//        cell.playerImage.clipsToBounds = true
+        
+        cell.playerImage.contentMode = UIViewContentMode.ScaleAspectFill
+        if indexPath.row%2==0
+        {
+            cell.backgroundColor = UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1)
+        }
+        else
+        {
+            cell.backgroundColor = UIColor.whiteColor()
+        }
+        
+        return cell
+        
+    }
+    
+//    func maskRoundedImage(image: UIImage, radius: Float) -> UIImage {
+//        let imageView: UIImageView = UIImageView(image: image)
+//        var layer: CALayer = CALayer()
+//        layer = imageView.layer
+//        
+//        layer.masksToBounds = true
+//        layer.cornerRadius = CGFloat(radius)
+//        
+//        UIGraphicsBeginImageContext(imageView.bounds.size)
+//        layer.renderInContext(UIGraphicsGetCurrentContext()!)
+//        let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//        return roundedImage
+//    }
+    
+    internal func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        
+        //let cell :UITableViewCell =  tableView.cellForRowAtIndexPath(indexPath)!
+         let playerInfoVC = storyboard?.instantiateViewControllerWithIdentifier("PlayerInfoViewController")as! PlayerInfoViewController
+        
+        playerdetails.playerImage = arr_ScoreDetail.valueForKey("player_image").objectAtIndex(indexPath.row) as! String
+        playerdetails.playerName =   arr_ScoreDetail.valueForKey("player_name").objectAtIndex(indexPath.row) as! String
+        playerdetails.weight =  arr_ScoreDetail.valueForKey("weight").objectAtIndex(indexPath.row) as! String
+        playerdetails.height =  arr_ScoreDetail.valueForKey("height").objectAtIndex(indexPath.row) as! String
+        playerdetails.birthDate =  arr_ScoreDetail.valueForKey("dob").objectAtIndex(indexPath.row) as! String
+        playerdetails.nationality =  arr_ScoreDetail.valueForKey("nationality").objectAtIndex(indexPath.row) as! String
+        playerdetails.nationFlag =  arr_ScoreDetail.valueForKey("team_flag").objectAtIndex(indexPath.row) as! String
+        playerdetails.teamFlag =  arr_ScoreDetail.valueForKey("team_logo").objectAtIndex(indexPath.row) as! String
+        playerdetails.team =  arr_ScoreDetail.valueForKey("team_name").objectAtIndex(indexPath.row) as! String
+        playerdetails.position =  arr_ScoreDetail.valueForKey("position").objectAtIndex(indexPath.row) as! String
+        playerdetails.shirtNumber =  arr_ScoreDetail.valueForKey("shirt_number").objectAtIndex(indexPath.row) as! String
+        
+        
+       
+        self.navigationController?.pushViewController(playerInfoVC, animated: true)
+    }
+    
+    
+    
 }
 
